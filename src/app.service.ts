b433b2ef5c9cb02eb8ebbfb15ee8cb30b57c3435
hwaung <102525421+kokim2022@users.noreply.github.com>
2023-11-02 17:00:32 +0630

    microservice implemented

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { GetUserRequest } from './get-user-request.dto';
 
 @Injectable()
 export class AppService {
+  private readonly users: any[] = [
+    {
+      userId: '123',
+      stripeUserId: 'ABCD',
+    },
+    {
+      userId: '456',
+      stripeUserId: 'EFGH',
+    },
+  ];
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUser(getUserRequest: GetUserRequest) {
+    console.log('getUserRequest', getUserRequest);
+    const stripeUser = this.users.find(
+      (user) => user.userId == getUserRequest.userId,
+    );
+    console.log('stripeUser', stripeUser);
+    return stripeUser;
   }
 }
